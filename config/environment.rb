3e41efc62d1dd7265cff9767169b530f191f79bf
Redfact <tantelyrandria@ymail.com>
2020-06-09 12:46:27 +0200

    add letter Opener

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,6 @@ require_relative 'application'
 
 # Initialize the Rails application.
 Rails.application.initialize!
+
+config.action_mailer.delivery_method = :letter_opener
+config.action_mailer.perform_deliveries = true
