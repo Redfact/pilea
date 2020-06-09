c4d7e9d55f3eeeecf5e765085a125c414e6ece81
Redfact <tantelyrandria@ymail.com>
2020-06-09 20:27:10 +0200

    merging dev

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,12 @@
+class UsersController < ApplicationController
+  before_action :authenticate_user!, only: [:show, :edit, :update]
+
+  def show
+    @user = User.find(params[:id])
+    if current_user == @user
+      render 'show'
+    else
+      redirect_to root_path
+    end
+  end
+end
