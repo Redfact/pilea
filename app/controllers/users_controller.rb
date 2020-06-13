fffbf024e6cc6c1507ecd3b64a969a7da6022cf9
AtlasRW <rw_protonmail.com>
2020-06-13 11:57:59 +0200

    test

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,16 +2,15 @@ class UsersController < ApplicationController
   before_action :authenticate_user!, only: [:show, :edit, :update]
 
   def show
-    @user = User.find(params[:id])
-    if current_user == @user
-      render 'show'
-    else
-      redirect_to root_path
-    end
+    @user = current_user
   end
 
   def edit
-    @user = User.find(params[:id])
+    @user = current_user
+  end
+
+  def update
+    @user = current_user
   end
 
 end
