9bbc49094a02d2c7d59ba417710e832c9225381d
AtlasRW <rw_protonmail.com>
2020-06-13 14:32:45 +0200

    test

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,7 +11,17 @@ class UsersController < ApplicationController
 
   def update
     @user = current_user
-    redirect_to user_path(current_user)
+    if @user.update(user_params)
+      redirect_to user_path(current_user)
+    else
+      render :edit
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:first_name, :last_name)
   end
 
 end
