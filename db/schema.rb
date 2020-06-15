ffcb9f4341617921a387d1ee0617098567c7c09d
AtlasRW <rw_protonmail.com>
2020-06-15 17:59:07 +0200

    and the schema !

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_06_15_142602) do
+ActiveRecord::Schema.define(version: 2020_06_15_155551) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -51,11 +51,9 @@ ActiveRecord::Schema.define(version: 2020_06_15_142602) do
     t.bigint "coin_id"
     t.decimal "price"
     t.bigint "volume"
-    t.bigint "market_cap"
     t.integer "interval", default: 0
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "timestamp"
     t.bigint "time"
     t.index ["coin_id"], name: "index_values_on_coin_id"
   end
