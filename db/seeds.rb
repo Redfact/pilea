82b5900fdbf79856644c66d0e9207f8d94f2d19a
AtlasRW <rw_protonmail.com>
2020-06-15 20:59:00 +0200

    seed coins

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,16 +1,36 @@
 require 'get_values'
 
 coins = {
- "bitcoin"=>["BTC","https://s2.coinmarketcap.com/static/img/coins/32x32/1.png"],
- "ethereum"=>["ETH","https://s2.coinmarketcap.com/static/img/coins/32x32/1027.png"],
- "ripple"=>["XRP","https://s2.coinmarketcap.com/static/img/coins/32x32/52.png"],
- "bitcoin-cash"=>["BCH","https://s2.coinmarketcap.com/static/img/coins/32x32/1831.png"],
- "litecoin"=>["LTC","https://s2.coinmarketcap.com/static/img/coins/32x32/2.png"],
- "binancecoin"=>["BNB","https://s2.coinmarketcap.com/static/img/coins/32x32/1839.png"],
- "eos"=>["EOS","https://s2.coinmarketcap.com/static/img/coins/32x32/1765.png"],
- "cardano"=>["ADA","https://s2.coinmarketcap.com/static/img/coins/32x32/2010.png"],
- "monero"=>["XMR","https://s2.coinmarketcap.com/static/img/coins/32x32/328.png"],
- "tron"=>["TRX","https://s2.coinmarketcap.com/static/img/coins/32x32/1958.png"]
+ "bitcoin"=>["btc","https://assets.coingecko.com/coins/images/1/large/bitcoin.png"],
+ "ethereum"=>["eth","https://assets.coingecko.com/coins/images/279/large/ethereum.png"],
+ "ripple"=>["xrp","https://assets.coingecko.com/coins/images/44/large/xrp.png"],
+ "litecoin"=>["ltc","https://assets.coingecko.com/coins/images/2/large/litecoin.png"],
+ "binancecoin"=>["bnb","https://assets.coingecko.com/coins/images/825/large/binance-coin-logo.png"],
+ "eos"=>["eos","https://assets.coingecko.com/coins/images/738/large/eos-eos-logo.png"],
+ "cardano"=>["ada","https://assets.coingecko.com/coins/images/975/large/cardano.png"],
+ "tezos"=>["xtz","https://assets.coingecko.com/coins/images/976/large/Tezos-logo.png"],
+ "stellar"=>["xlm","https://assets.coingecko.com/coins/images/100/large/Stellar_symbol_black_RGB.png"],
+ "monero"=>["xmr","https://assets.coingecko.com/coins/images/69/large/monero_logo.png"],
+ "tron"=>["trx","https://assets.coingecko.com/coins/images/1094/large/tron-logo.png"],
+ "neo"=>["neo","https://assets.coingecko.com/coins/images/480/large/neo.png"],
+ "dash"=>["dash","https://assets.coingecko.com/coins/images/19/large/dash-logo.png"],
+ "iota"=>["miota","https://assets.coingecko.com/coins/images/692/large/IOTA_Logo.png"],
+ "cosmos"=>["atom","https://assets.coingecko.com/coins/images/1481/large/cosmos_hub.png"],
+ "zcash"=>["zec","https://assets.coingecko.com/coins/images/486/large/circle-zcash-color.png"],
+ "vechain"=>["vet","https://assets.coingecko.com/coins/images/1167/large/VeChain-Logo-768x725.png"],
+ "nem"=>["xem","https://assets.coingecko.com/coins/images/242/large/nem.png"],
+ "ontology"=>["ont","https://assets.coingecko.com/coins/images/3447/large/ONT.png"],
+ "dogecoin"=>["doge","https://assets.coingecko.com/coins/images/5/large/dogecoin.png"],
+ "digibyte"=>["dgb","https://assets.coingecko.com/coins/images/63/large/digibyte.png"],
+ "zilliqa"=>["zil","https://assets.coingecko.com/coins/images/2687/large/Zilliqa-logo.png"],
+ "decred"=>["dcr","https://assets.coingecko.com/coins/images/329/large/decred.png"],
+ "algorand"=>["algo","https://assets.coingecko.com/coins/images/4380/large/download.png"],
+ "icon"=>["icx","https://assets.coingecko.com/coins/images/1060/large/icon-icx-logo.png"],
+ "qtum"=>["qtum","https://assets.coingecko.com/coins/images/684/large/qtum.png"],
+ "lisk"=>["lsk","https://assets.coingecko.com/coins/images/385/large/Lisk_Symbol_-_Blue.png"],
+ "verge"=>["xvg","https://assets.coingecko.com/coins/images/203/large/verge-symbol-color_logo.png"],
+ "nano"=>["nano","https://assets.coingecko.com/coins/images/756/large/nano-coin-logo.png"],
+ "waves"=>["waves","https://assets.coingecko.com/coins/images/425/large/waves.png"]
 }
 
 Coin.destroy_all
