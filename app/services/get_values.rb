b65d27c08125ccc538e53b36a249d763d14e5c32
Redfact <tantelyrandria@ymail.com>
2020-06-17 01:33:18 +0200

    Add methodes to get only new values (hourly,daly)

--- app/services/get_values.rb
+++ app/services/get_values.rb
@@ -1,18 +1,19 @@
 class GetValues
-   attr_accessor :daily, :coin_name ,:coin ,:hourly
+   attr_accessor :daily, :coin_name ,:coin ,:hourly,:lastDateDaily,:lastDateHourly
 
    def initialize(pcoin_name) 
       @daily = Hash.new
       @hourly = Hash.new
       @coin_name = pcoin_name
       @coin = Coin.find_by(name:pcoin_name)
-
+      @lastDateDaily =  @coin.daily.last.time
+      @lastDateHourly = @coin.hourly.last.time
       # Get all existing values if DB is empty
       # Or just get all new values if database is already filled
       if(!@coin.values.empty?)
-            @daily = update_daily
-            @hourly = update_hourly
-         else
+         @hourly = update_hourly
+         @daily = update_daily
+      else
             @daily = history_daily
             @hourly  = history_hourly 
       end  
@@ -22,56 +23,18 @@ class GetValues
 
    #Get all existing values of the current coin daily since today 
    def history_daily
-      response = RestClient.get("https://api.coingecko.com/api/v3/coins/#{coin_name}/market_chart?vs_currency=usd&days=max")
+      response = RestClient.get("https://api.coingecko.com/api/v3/coins/#{@coin_name}/market_chart?vs_currency=usd&days=max")
       tmp_hash = JSON.parse(response)
       return tmp_hash
    end
 
    #Get all existing values of the current coin from now to 3 months ago
    def history_hourly
-      response = RestClient.get("https://api.coingecko.com/api/v3/coins/#{coin_name}/market_chart?vs_currency=usd&days=90")
+      response = RestClient.get("https://api.coingecko.com/api/v3/coins/#{@coin_name}/market_chart?vs_currency=usd&days=90")
       tmp_hash = JSON.parse(response)
       return tmp_hash
    end
 
-
-   # Return hash with new values from last update to now.
-   # /!\ Granularity depends on how big the duration (between now and lastdate) of time is.
-   # Every 1 min if within 1 days , hourly between 1 to 90 days and daily if above 90 days.
-   # def update_hourly
-   #    # lastdate = (coin.hourly.last.time/1000).to_s
-   #    lastdate = "1592215236"
-   #    now = DateTime.now.strftime('%s')
-
-   #    # Get the duration in days 
-   #    duration = ( DateTime.strptime(now,'%s') - DateTime.strptime(lastdate,'%s') ).to_i
-   #    puts "duration:",duration
-
-   #    if( duration < 2 )
-   #       url="https://api.coingecko.com/api/v3/coins/#{coin_name}/market_chart?vs_currency=usd&days=2"
-   #    else
-   #       url="https://api.coingecko.com/api/v3/coins/#{coin_name}/market_chart/range?vs_currency=usd&from=#{lastdate}&to=#{now}"   
-   #    end
-   #    puts url 
-   #    request=RestClient.get(url)
-   #    return JSON.parse(request)
-   # end
-
-   # Return hash of last new values 
-   def update_hourly
-      lastimestmp = coin.values.last.time
-      hourlyhash = history_hourly
-      prices = hourlyhash['prices']
-      return prices
-   
-   end
-   
-
-   def update_daily
-
-
-   end
-   
 
    # Get all values per value type  (volumes,market cap,prices) in hash (daily or hourly)
    #Return 1 dimension array with the value type 
@@ -91,6 +54,47 @@ class GetValues
          array_res<<index[0]
       }
       return array_res 
+   end
+
+   # Get a sub array of values from last database date to most recent date 
+   # Return 1 dimension array of values
+   def get_sub_array(array,lastime)
+
+      #Reduce the number of iteration if we iterate from the end
+      array = array.reverse()
+      # count the number of iteration 
+      iter=0
+      array.each do |elem|
+         #duration between the last date (in DB) and the new date readed
+         deltatime = elem[0] - lastime
+            if( deltatime < 0 )  
+               break
+            else
+               iter +=1 
+            end
+      end 
+      return array.first(iter).reverse()
+   end
+
+   # Return hash of last new values hourly 
+   def update_hourly
+      response = RestClient.get("https://api.coingecko.com/api/v3/coins/#{@coin_name}/market_chart?vs_currency=usd&days=90")
+      hourly = JSON.parse(response)
+      hourly['prices']=get_sub_array(hourly['prices'],@lastDateHourly)
+      hourly['market_caps']=get_sub_array(hourly['market_caps'],@lastDateHourly)
+      hourly['total_volumes']=get_sub_array(hourly['total_volumes'],@lastDateHourly)
+      return hourly ; 
+   end 
+
+   # Return hash of last new values hourly 
+   def update_daily
+      # TODO : Change max when the worker will be setted up 
+      response = RestClient.get("https://api.coingecko.com/api/v3/coins/#{@coin_name}/market_chart?vs_currency=usd&days=max")
+      hourly = JSON.parse(response)
+      hourly['prices']=get_sub_array(hourly['prices'],@lastDateDaily)
+      hourly['market_caps']=get_sub_array(hourly['market_caps'],@lastDateDaily)
+      hourly['total_volumes']=get_sub_array(hourly['total_volumes'],@lastDateDaily)
+      return hourly ; 
    end
 
 end
