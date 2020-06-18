f07c7244f58cbe55e51e15965432ae55dd7b408f
TonyStark992 <maxime.castillo@gmail.com>
2020-06-18 17:15:48 +0200

    FillDatabase by_interval_type added

--- app/services/fill_database.rb
+++ app/services/fill_database.rb
@@ -1,8 +1,20 @@
 class FillDatabase
   attr_accessor :coin
 
-  def initialize(coin_name)
+  def initialize
     @coin = coin_name
+  end
+
+  def by_interval_type(interval)
+    interval_equivalence_num = {
+      "minutely"=>0,
+      "hourly"=>1,
+      "daily"=>2,
+    }
+    current_coin = coin.name
+    values = GetValues.new(current_coin).history(interval)
+    SaveValues.new(@coin, interval, 2).perform
+    hourly
   end
 
 end
