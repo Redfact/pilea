e4f0f873ce90287603153a7b174041769a50645d
TonyStark992 <maxime.castillo@gmail.com>
2020-06-19 17:39:35 +0200

    Testing Updating method

--- app/services/fill_database.rb
+++ app/services/fill_database.rb
@@ -15,4 +15,21 @@ class FillDatabase
     }
   end
 
+  def update_by_interval_type(interval)
+    interval_equivalence_num = {
+      "minutely"=>0,
+      "hourly"=>1,
+      "daily"=>2,
+    }
+    all_coins = Coin.all
+    all_coins.each { |coin|
+      puts "Updating database with #{interval} values of '#{coin.name}'..."
+      sleep 1
+      values = GetValues.new(coin.name).update(interval)
+      SaveValues.new(coin.name, values, interval_equivalence_num[interval]).perform
+      puts "...Done!"
+      sleep 1
+    }
+  end
+
 end
