fcb471de9cfa6a8874c10aaa54c63363bd36d266
TonyStark992 <maxime.castillo@gmail.com>
2020-06-19 17:53:34 +0200

    Refactoring FillDatabase with if condition

--- app/services/fill_database.rb
+++ app/services/fill_database.rb
@@ -8,27 +8,17 @@ class FillDatabase
     }
     all_coins = Coin.all
     all_coins.each { |coin|
-      puts "Filling database with #{interval} values of '#{coin.name}'..."
-      values = GetValues.new(coin.name).history(interval)
-      SaveValues.new(coin.name, values, interval_equivalence_num[interval]).perform
+      if coin.values.empty?
+        puts "No existing #{interval} value for '#{coin.name}' ! Filling database for the first time'..."
+        sleep 1
+        values = GetValues.new(coin.name).history(interval)
+      else
+        puts "Updating database with #{interval} values of '#{coin.name}'..."
+        sleep 1
+        values = GetValues.new(coin.name).update(interval)
+      end
+        SaveValues.new(coin.name, values, interval_equivalence_num[interval]).perform
       puts "...Done!"
-    }
-  end
-
-  def update_by_interval_type(interval)
-    interval_equivalence_num = {
-      "minutely"=>0,
-      "hourly"=>1,
-      "daily"=>2,
-    }
-    all_coins = Coin.all
-    all_coins.each { |coin|
-      puts "Updating database with #{interval} values of '#{coin.name}'..."
-      sleep 1
-      values = GetValues.new(coin.name).update(interval)
-      SaveValues.new(coin.name, values, interval_equivalence_num[interval]).perform
-      puts "...Done!"
-      sleep 1
     }
   end
 
