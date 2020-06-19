871e51b7864f7fc63cac083e502956731162daa8
AtlasRW <rw_protonmail.com>
2020-06-19 18:16:16 +0200

    STATICS & OTHERS PATHS

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   root 'coins#index'
+  get '/team', to: 'statics#team'
+  get '/project', to: 'statics#project'
   resources :selections
   resources :coins, only: [:show]
   devise_for :users
