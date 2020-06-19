78f777a396f5d949b04f1f06839253ff91e65062
TonyStark992 <maxime.castillo@gmail.com>
2020-06-19 18:20:21 +0200

    Services updated

--- app/services/fill_database.rb
+++ app/services/fill_database.rb
@@ -7,9 +7,12 @@ class FillDatabase
       "daily"=>2,
     }
     all_coins = Coin.all
+    puts "-"*100
+    puts "Starting to update all coins #{interval} values..."
     all_coins.each { |coin|
       if coin.values.empty?
-        puts "No existing #{interval} value for '#{coin.name}' ! Filling database for the first time..."
+        puts "No existing #{interval} value for '#{coin.name}' !"
+        puts "Filling database for the first time..."
         sleep 2
         values = GetValues.new(coin.name).history(interval)
       else
@@ -20,7 +23,7 @@ class FillDatabase
         SaveValues.new(coin.name, values, interval_equivalence_num[interval]).perform
       sleep 0.5
     }
-    puts "-"*80
+    puts "-"*100
     puts "The database has been updated with #{interval} value for all coins."
   end
 
