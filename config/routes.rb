8c62e772dffba638173136c8d31e2c704ec6e6a5
AtlasRW <rw_protonmail.com>
2020-06-19 23:41:33 +0200

    ADMIN DASHBOARD FIX

--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,17 @@
 Rails.application.routes.draw do
-  namespace :admin do
-      resources :users
-      resources :coins
-      resources :selections
-      resources :values
-
-      root to: "users#index"
-    end
   root 'coins#index'
   get '/team', to: 'statics#team'
   get '/project', to: 'statics#project'
-  resources :selections
+
   resources :coins, only: [:show]
   devise_for :users
   resources :users, only: [:show, :edit, :update]
+  resources :selections
+
+  namespace :admin do
+    resources :users
+    resources :coins
+    resources :values
+    root to: "users#index"
+  end
 end
