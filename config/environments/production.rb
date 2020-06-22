5260e5b852c48a485db4b18eb56d6a548d2cbd96
AtlasRW <rw_protonmail.com>
2020-06-22 02:59:54 +0200

    DEVISE FIX 2

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -93,7 +93,7 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
 
   config.action_mailer.delivery_method = :mailjet
-
+  config.action_mailer.default_url_options = { :host => ENV['MAILER_URL'] }
   config.action_mailer.perform_deliveries = true
   
 end
